f6e2b70b396c35fdfba11caca7e96375b8569141
Richie Glennon <glennonr@moravian.edu>
2025-02-21 11:21:45 -0500

    start webp conversion

--- src/app/merch/page.tsx
+++ src/app/merch/page.tsx
@@ -7,19 +7,19 @@ export default function Merch() {
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6 justify-center items-center">
           {/* Shirt 1 */}
           <div className="bg-black p-4 rounded-lg shadow-lg">
-            <img src="/merch/tour shirt.png" alt="Parkways Shirt 1" className="w-full rounded-lg" />
+            <img src="/merch/tour shirt.webp" alt="Parkways Shirt 1" className="w-full rounded-lg" />
             <p className="mt-2 text-lg font-bold">Original Shirt</p>
           </div>
           
           {/* Shirt 2 */}
           <div className="bg-black p-4 rounded-lg shadow-lg">
-            <img src="/merch/KellyGreen.png" alt="Parkways Kelly Green" className="w-full rounded-lg" />
+            <img src="/merch/KellyGreen.webp" alt="Parkways Kelly Green" className="w-full rounded-lg" />
             <p className="mt-2 text-lg font-bold">Kelly Green Shirt</p>
           </div>
           
           {/* Sticker */}
           <div className="bg-black p-4 rounded-lg shadow-lg">
-            <img src="/merch/Stickers.png" alt="Parkways Sticker" className="w-full rounded-lg" />
+            <img src="/merch/Stickers.webp" alt="Parkways Sticker" className="w-full rounded-lg" />
             <p className="mt-2 text-lg font-bold">Parkways Sticker</p>
           </div>
         </div>
